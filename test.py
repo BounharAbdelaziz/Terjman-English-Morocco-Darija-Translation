d5faec387961cac620597c1e0086ed9fa2d94cfb
BounharAbdelaziz <bounhar.abdelaziz@gmail.com>
2025-01-20 17:02:43 +0100

    test code

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import os
 os.environ["TORCH_USE_CUDA_DSA"] = "1" # Enable CUDA DSA
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 if __name__ == "__main__":
 
+    # Define model path
+    MODEL_PATH = "atlasia/Terjman-Large-v2"
+    
     # Load the tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained("atlasia/Terjman-Large-v2")
-    model = AutoModelForSeq2SeqLM.from_pretrained("atlasia/Terjman-Large-v2")
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+    model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_PATH)
 
     # Define your Moroccan Darija Arabizi text
     input_text = "Your english text goes here."
